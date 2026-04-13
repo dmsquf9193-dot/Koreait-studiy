861776238c831209b7d5e94b045908a1bba8e354
silvery <dmsquf9193@gmail.com>
2026-04-13 18:09:24 +0900

    ai-project
    gi

---
+++ ai-project-1/src/main/java/kr/co/ai_project/AiProject1Application.java
@@ -0,0 +1,13 @@
+package kr.co.ai_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProject1Application.class, args);
+	}
+
+}
